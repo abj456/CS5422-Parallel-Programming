8f834be9060e418a001a937dddb0f9d7133fc631
abj456 <tommy51315@gmail.com>
2024-11-30 23:57:03 +0800

    trivial modification of cal() in hw3-1.cc

--- hw3/hw3-1/hw3-1.cc
+++ hw3/hw3-1/hw3-1.cc
@@ -132,22 +132,22 @@ void cal(
 
     #pragma omp parallel
     {
+        // To calculate B*B elements in the block (b_i, b_j)
+        // For each block, it need to compute B times
         #pragma omp for schedule(dynamic, CHUNK_SIZE) collapse(2)
         for (int b_i = block_start_x; b_i < block_end_x; ++b_i) {
             for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
-                // To calculate B*B elements in the block (b_i, b_j)
-                // For each block, it need to compute B times
+                // To calculate original index of elements in the block (b_i, b_j)
+                // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
+                int block_internal_start_x = b_i * B;
+                int block_internal_end_x = (b_i + 1) * B;
+                int block_internal_start_y = b_j * B;
+                int block_internal_end_y = (b_j + 1) * B;
+
+                if (block_internal_end_x > n) block_internal_end_x = n;
+                if (block_internal_end_y > n) block_internal_end_y = n;
+
                 for (int k = k_start; k < k_end; ++k) {
-                    // To calculate original index of elements in the block (b_i, b_j)
-                    // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
-                    int block_internal_start_x = b_i * B;
-                    int block_internal_end_x = (b_i + 1) * B;
-                    int block_internal_start_y = b_j * B;
-                    int block_internal_end_y = (b_j + 1) * B;
-
-                    if (block_internal_end_x > n) block_internal_end_x = n;
-                    if (block_internal_end_y > n) block_internal_end_y = n;
-
                     for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
                         for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
                             if (Dist[i][k] + Dist[k][j] < Dist[i][j]) {

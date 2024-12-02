9ffd475c915f0093ed3e4a101b2bcf499a4a85f8
Student 058 pp24 <pp24s058@apollo-gpu.lsalab.cs.nthu.edu.tw>
2024-12-02 17:59:37 +0800

    hw3-2 BF=32 shared memory completed

--- hw3/hw3-2/hw3-2.cu
+++ hw3/hw3-2/hw3-2.cu
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <cuda.h>
 #include <assert.h>
+#include <omp.h>
 
 const int INF = ((1 << 30) - 1);
 
 //======================
 #define DEV_NO 0
 #define BF 32
+#define STRINGIFY(x) #x
+#define PRAGMA_UNROLL(x) _Pragma(STRINGIFY(unroll x))
+
 cudaDeviceProp prop;
 int n, m, NUM_THREADS;
 int org_n;
 int *Dist_host;
+
 void print_matrix(int V, int n, int *Dist);
 
 void input(char* infile) {
@@ -26,15 +31,15 @@ void input(char* infile) {
 
     Dist_host = (int*)malloc(n * n * sizeof(int));
 
+    omp_set_num_threads(8);
+    #pragma omp parallel for schedule(dynamic, BF) collapse(2)
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            // printf("%d %d assignment start\n", i, j);
             if (i == j) {
                 Dist_host[i * n + j] = 0;
             } else {
                 Dist_host[i * n + j] = INF;
             }
-            // printf("%d %d assignment OK\n", i, j);
         }
     }
 
@@ -65,7 +70,7 @@ int ceil(int a, int b) { return (a + b - 1) / b; }
 
 __global__ void cal(
     int n, int *Dist, int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height){
-    
+    /* common-use block_FW cal() */
     // printf("blockIdx.x = %d, blockIdx.y = %d, threadIdx.x = %d, blockDim.x = %d\n", blockIdx.x, blockIdx.y, threadIdx.x, blockDim.x);
     int block_end_x = block_start_x + block_height;
     int block_end_y = block_start_y + block_width;
@@ -133,13 +138,17 @@ __global__ void cal_phase1(int n, int *Dist, int B, int Round) {
     shared_block[thread_i][thread_j] = Dist[global_i * n + global_j];
     __syncthreads();
 
+    /* no shared memory */
     // for(int k = Round * B; k < n && k < (Round + 1) * B; ++k){
+    //     Dist[global_i * n + global_j] = min(Dist[global_i * n + global_j], (Dist[global_i * n + k] + Dist[k * n + global_j]));
+    //     __syncthreads();
+    // }
+
     #pragma unroll 32
-    for(int k = 0; k < BF && (Round * BF + k) < n; ++k) {
-        // Dist[global_i * n + global_j] = min(Dist[global_i * n + global_j], (Dist[global_i * n + k] + Dist[k * n + global_j]));
+    for(int k = 0; k < BF; ++k) {
         shared_block[thread_i][thread_j] = min(shared_block[thread_i][thread_j], 
                                               (shared_block[thread_i][k] + shared_block[k][thread_j]));
-        __syncthreads();
+        // __syncthreads();
     }
     Dist[global_i * n + global_j] = shared_block[thread_i][thread_j];
 }
@@ -171,21 +180,22 @@ __global__ void cal_phase2(int n, int *Dist, int B, int Round) {
     
     __syncthreads();
     
+    /* no shared memory */
     // for(int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
-    //     // Dist[hz_i * n + hz_j] = min(Dist[hz_i * n + hz_j], (Dist[hz_i * n + k] + Dist[k * n + hz_j]));
-    //     shared_hz[thread_i][thread_j] = min(shared_hz[thread_i][thread_j], (Dist[hz_i * n + k] + Dist[k * n + hz_j]));
-    //     // Dist[vt_i * n + vt_j] = min(Dist[vt_i * n + vt_j], (Dist[vt_i * n + k] + Dist[k * n + vt_j]));
-    //     shared_vt[thread_i][thread_j] = min(shared_vt[thread_i][thread_j], (Dist[vt_i * n + k] + Dist[k * n + vt_j]));
+    //     Dist[hz_i * n + hz_j] = min(Dist[hz_i * n + hz_j], (Dist[hz_i * n + k] + Dist[k * n + hz_j]));
+    //     Dist[vt_i * n + vt_j] = min(Dist[vt_i * n + vt_j], (Dist[vt_i * n + k] + Dist[k * n + vt_j]));
     //     __syncthreads();
     // }
 
+    // #pragma unroll BF
+    PRAGMA_UNROLL(BF)
     for(int k = 0; k < BF; ++k) {
         shared_hz[thread_i][thread_j] = min(shared_hz[thread_i][thread_j], 
                                             shared_pivot[thread_i][k] + shared_hz[k][thread_j]);
     
         shared_vt[thread_i][thread_j] = min(shared_vt[thread_i][thread_j],
                                             shared_vt[thread_i][k] + shared_pivot[k][thread_j]);
-        __syncthreads();
+        // __syncthreads();
     }
     
     Dist[hz_i * n + hz_j] = shared_hz[thread_i][thread_j];
@@ -198,34 +208,53 @@ __global__ void cal_phase3(int n, int *Dist, int B, int Round) {
     int b_i = blockIdx.x;
     int b_j = blockIdx.y;
 
-    if(b_i == Round || b_j == Round) return;
-
-    int global_i = b_i * BF + thread_i;
-    int global_j = b_j * BF + thread_j;
+    if(b_i == Round || b_j == Round) return; // skip pivot, pivot hz, pivot vt
 
     __shared__ int shared_hz[BF][BF];
     __shared__ int shared_vt[BF][BF];
 
-    
-    for(int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
-        Dist[global_i * n + global_j] = min(Dist[global_i * n + global_j], (Dist[global_i * n + k] + Dist[k * n + global_j]));
+    int global_i = b_i * BF + thread_i;
+    int global_j = b_j * BF + thread_j;
+    /* idx of pivot hz block */
+    int hz_i = Round * BF + thread_i;
+    int hz_j = b_j * BF + thread_j;
+    /* idx of pivot vt block */
+    int vt_i = b_i * BF + thread_i;
+    int vt_j = Round * BF + thread_j;
 
-        __syncthreads();
+    shared_hz[thread_i][thread_j] = Dist[hz_i * n + hz_j];
+    shared_vt[thread_i][thread_j] = Dist[vt_i * n + vt_j];
+    __syncthreads();
+    
+    /* no shared memory */
+    // for(int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
+    //     Dist[global_i * n + global_j] = min(Dist[global_i * n + global_j], (Dist[global_i * n + k] + Dist[k * n + global_j]));
+
+    //     __syncthreads();
+    // }
+
+    int tmp_data = Dist[global_i * n + global_j];
+    // #pragma unroll BF
+    PRAGMA_UNROLL(BF)
+    for(int k = 0; k < BF; ++k) {
+        tmp_data = min(tmp_data, shared_vt[thread_i][k] + shared_hz[k][thread_j]);
     }
+    
+    Dist[global_i * n + global_j] = tmp_data;
 }
 
 void block_FW(int n, int B, int *Dist) {
     int round = ceil(n, B);
     dim3 threadsPerBlock(BF, BF);
     dim3 blocksPerGrid(round, round);
-    size_t shared_block_size = BF * BF * sizeof(int);
+    // size_t shared_block_size = BF * BF * sizeof(int);
 
     for(int r = 0; r < round; ++r) {
-        printf("%d %d\n", r, round);
+        // printf("%d %d\n", r, round);
         // fflush(stdout);
         /* Phase 1*/
         // cal<<<1, threadsPerBlock>>>(n, Dist, B, r, r, r, 1, 1);
-        cal_phase1<<<1, threadsPerBlock>>>(n, Dist, B, r);
+        cal_phase1<<<1, threadsPerBlock>>>(n, Dist, BF, r);
         // cudaDeviceSynchronize();
 
         /* Phase 2*/
@@ -248,24 +277,21 @@ int main(int argc, char* argv[]) {
     int B = BF;
 
     cudaGetDeviceProperties(&prop, DEV_NO);
-    printf("maxThreasPerBlock = %d, sharedMemPerBlock = %d\n", prop.maxThreadsPerBlock, prop.sharedMemPerBlock);
+    // printf("maxThreasPerBlock = %d, sharedMemPerBlock = %d\n", prop.maxThreadsPerBlock, prop.sharedMemPerBlock);
 
     // Dist_host = (int*)malloc(V * V * sizeof(int));
     input(argv[1]);
     assert(Dist_host != nullptr);
-    // print_matrix(org_n, Dist_host);
 
     int *Dist_dev;
     cudaMalloc(&Dist_dev, n * n * sizeof(int));
     cudaMemcpy(Dist_dev, Dist_host, n * n * sizeof(int), cudaMemcpyHostToDevice);
     assert(Dist_dev != nullptr);
-    // gpu_print_matrix<<<1, 1>>>(org_n, n, Dist_dev);
 
     // block_FW(B);
     block_FW(n, B, Dist_dev);
     
     cudaMemcpy(Dist_host, Dist_dev, n * n * sizeof(int), cudaMemcpyDeviceToHost);
-    // print_matrix(org_n, n, Dist_host);
     output(argv[2]);
     cudaFreeHost(Dist_host);
     cudaFree(Dist_dev);
